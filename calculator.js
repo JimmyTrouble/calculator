3bc6929701494851243b76992715a5da120814cf
JimmyTrouble <114571635+JimmyTrouble@users.noreply.github.com>
2023-06-09 18:13:23 +1000

    first commit

---
+++ calculator.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+  let sum = num1 + num2;
+  res.send(sum.toString());
+});
+
+app.listen(3000, function () {
+  console.log("running");
+});
